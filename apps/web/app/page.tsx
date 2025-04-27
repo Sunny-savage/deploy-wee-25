c4a56df97f8a4cd02c6b0c0efc8ed25e7cf0f235
Sunny-savage <vishal200231@gmail.com>
2025-04-28 01:43:02 +0530

    stageing pe

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import { client } from "@repo/db/client";
 export default async function Home() {
   const user = await client.user.findFirst();
   return ( 
-    <div>fristName : stage
+    <div>fristName : staging hai ye
       {user?.username}
       LastName : 
       {user?.password}
